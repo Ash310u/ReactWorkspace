97eb21af127e58acc0a0e9ef660ca3a88e6de738
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-12-14 18:17:38 +0530

    Commited: a component called 'App.js' and rendered it inside index.js

---
+++ jsx/src/App.js
@@ -0,0 +1,16 @@
+// A component
+function App() {
+    return (
+        <textarea
+            autoFocus
+            spellCheck
+            style={{
+                fontFamily:'monospace',
+                color:'green',
+                background:'#424242'
+            }}
+        />
+    )
+}
+
+export default App;
