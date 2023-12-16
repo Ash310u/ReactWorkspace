9ea0f5815c81233641ee150d3ddb6b169a09038c
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-12-16 16:17:50 +0530

    Commited: Added Some little styling

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -6,16 +6,25 @@ import SiriImg from './images/siri.png'
 const App = () => {
     return (
         <div>
-            <div>Personal Digital Assistants</div>
+            <section className='hero is-primary'>
+                <div className='hero-body'>
+                    <p className='title'>
+                        Personal Digital Assistants
+                    </p>
+                </div>
+            </section>
+
             <div className='container'>
                 <section className='section'>
                     <div className='columns'>
                         <div className='column is-4'>
                             <ProfileCard title='Alexa'  handle='@alexa99' img={AlexaImg} />
                         </div>
+
                         <div className='column is-4'>
                             <ProfileCard title='Cortana' handle='@cortana32' img={CortanaImg}/>          
                         </div>
+                        
                         <div className='column is-4'>
                             <ProfileCard title='Siri' handle='@siri01' img={SiriImg}/>          
                         </div>
