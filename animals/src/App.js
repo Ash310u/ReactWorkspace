e4ffdd518410b22ea8958d167ae788fbc1326708
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-12-17 16:45:35 +0530

    Commited: used 'useState' to create a counter

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,10 +1,16 @@
+import { useState } from "react"
+
 const App = () => {
+    const [count, setCount] =  useState(0)
+
+    const handleCounter = () => {
+        setCount(count + 1)
+    }
+
     return (
         <div>
-            Show animal list here!
-            <button onClick={() => console.log('it works')}>
-                Click
-            </button>
+            <button onClick={handleCounter}>Click</button>
+            <div>Number of animal: {count}</div>
         </div>
     )
 }
