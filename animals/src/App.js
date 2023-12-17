090b74ab2de4eb6c45156876469b790a9a406667
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-12-17 15:33:24 +0530

    Commited: refactored the func again

--- animals/src/App.js
+++ animals/src/App.js
@@ -2,11 +2,7 @@ const App = () => {
     return (
         <div>
             Show animal list here!
-            <button onClick={
-                () => {
-                    console.log('it works')
-                }
-            }>
+            <button onClick={() => console.log('it works')}>
                 Click
             </button>
         </div>
