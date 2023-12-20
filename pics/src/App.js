32272f71d14cfff805141a2b91880c24b691a0f8
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-12-20 09:47:11 +0530

    Commited: a onSubmit prop with a handleSubmit function to pass data from the children to the parent element using onClick on searchBar button

--- pics/src/App.js
+++ pics/src/App.js
@@ -1,5 +1,16 @@
+import ImageList from "./components/ImageList"
+import SearchBar from "./components/SearchBar"
+import searchImages from './api';
+
 const App = () => {
-    return <div>App</div>
+    const handleSubmit = (term) => {
+        console.log(term)
+    }
+    return (
+        <div>
+            <SearchBar onSubmit={handleSubmit} />
+        </div>
+    )
 }
 
 export default App
--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,5 +1,17 @@
-const SearchBar = () => {
-    return <div>SearchBar</div>
+import { useState } from "react";
+
+const SearchBar = ({ onSubmit }) => {
+    const [intialValue, setIntialValue] = useState('')
+    return (
+        <div>
+            <input 
+                type="text"
+                value={intialValue}
+                onChange={e => setIntialValue(e.target.value)}
+            />
+            <button onClick={onSubmit(intialValue)}>Search</button>
+        </div>
+    )
 }
 
 export default SearchBar
