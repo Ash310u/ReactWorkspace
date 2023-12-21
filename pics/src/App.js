3e3d9749e7c52f49661cf3bc91e32803cc08d552
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-12-22 04:49:02 +0530

    Commited: a async function to receive the searchImage result and imported the ImageList component

--- pics/src/App.js
+++ pics/src/App.js
@@ -1,13 +1,16 @@
+import ImageList from "./components/ImageList";
 import SearchBar from "./components/SearchBar";
 import searchImages from './api';
 
 const App = () => {
-    const handleSubmit = (term) => {
-        searchImages(term)
+    const handleSubmit = async (term) => {
+        const result = await searchImages(term)
+        console.log(result)
     }
     return (
         <div>
             <SearchBar Submit={handleSubmit} />
+            <ImageList />
         </div>
     )
 }
