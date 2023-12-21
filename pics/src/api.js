20b905efcc63254cf490592ac25758600805549c
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-12-22 04:38:40 +0530

    Commited: a comment

--- pics/src/api.js
+++ pics/src/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
+// Using axios to fetch data from unsplash api
 const searchImages = async (term) => {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
         headers: {
