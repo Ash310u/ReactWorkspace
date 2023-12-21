7c60ead2979b72dc4d5563d53dc0092945083833
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-12-21 23:01:34 +0530

    Cleanup: cleaned up the code

--- pics/src/App.js
+++ pics/src/App.js
@@ -1,6 +1,4 @@
-import ImageList from "./components/ImageList";
 import SearchBar from "./components/SearchBar";
-import searchImages from './api';
 
 const App = () => {
     const handleSubmit = (term) => {
