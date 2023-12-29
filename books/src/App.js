ed82cf3936107e84d1ada4fd231cf1a1c47977df
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-12-29 19:26:23 +0530

    Commited: editBookId function

--- books/src/App.js
+++ books/src/App.js
@@ -20,9 +20,18 @@ function App() {
         })
         setBooks(updatedBooks);
     }
+    const editBookById = (title, id) => {
+        const updatedBooks = books.map((book) => {
+            if(book.id === id) {
+                return {...book, title}
+            }
+            return book
+        })
+        setBooks(updatedBooks);
+    }
     return (
         <div>
-            <BookList books={books} onDelete={deleteBookById} />
+            <BookList books={books} onDelete={deleteBookById} onEdit={editBookById} />
             <BookCreate onCreate={createBook} />
         </div>
     )
