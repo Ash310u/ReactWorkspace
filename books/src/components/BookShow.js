85b134010261d6dc5ea7c8df9f43cae187369d95
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-12-31 08:56:47 +0530

    Commited: showEdit state and created a function called handleEditClick & passed it to the onClick of Edit button

--- books/src/components/BookShow.js
+++ books/src/components/BookShow.js
@@ -1,15 +1,22 @@
+import { useState } from "react"
+
 function BookShow({ book, onDelete, onEdit}) {
-    const handleChange = () => {
-        onEdit()
-    }
-    const handleClick = () => {
+    const [showEdit, setShowEdit] = useState(false)
+
+    const handleDeleteClick = () => {
         onDelete(book.id)
+    }
+    const handleEditClick = () => {
+        setShowEdit(!showEdit);
     }
     return (
         <div className="book-show">
             {book.title}
             <div className="actions">
-                <button className="delete" onClick={handleClick}>
+                <button className="edit" onClick={handleEditClick}>
+                    Edit
+                </button>
+                <button className="delete" onClick={handleDeleteClick}>
                     Delete
                 </button>
             </div>
