c162f101915000555400a639486d49df829a454b
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-01-04 11:29:07 +0530

    Commited: BookEdit form component

--- books/src/components/BookEdit.js
+++ books/src/components/BookEdit.js
@@ -1,7 +1,22 @@
-function BookEdit() {
+import { useState } from "react";
+
+function BookEdit({ book, onSubmit }) {
+    const [value, setValue] = useState(book.title);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onSubmit(book.id, value);
+    };
     return (
-        <div>BookEdit</div>
-    )
+        <form onSubmit={handleSubmit} className="book-edit">
+            <label>Title</label>
+            <input
+                className="input"
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+            />
+            <button className="button is-primary">Save</button>
+        </form>
+    );
 }
 
-export default BookEdit
+export default BookEdit;
