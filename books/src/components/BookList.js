ad0603f471e511b0c703782d7f0b7703b045607b
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-01-05 07:11:48 +0530

    feat: Using Context To send some value through a centralized store and print it inside BookList component for testing

--- books/src/components/BookList.js
+++ books/src/components/BookList.js
@@ -1,12 +1,19 @@
+import { useContext } from "react"
+import BooksContext from "../context/books"
 import BookShow from "./BookShow"
 
 function BookList({ books, onDelete, onEdit }) {
+    const value = useContext(BooksContext)
+
     const listing = books.map(book => {
         return <BookShow book={book} key={book.id} onDelete={onDelete} onEdit={onEdit} />
     })
 
     return (
-        <div className="book-list">{listing}</div>
+        <div className="book-list">
+            {value}
+            {listing}
+        </div>
     )
 }
 
