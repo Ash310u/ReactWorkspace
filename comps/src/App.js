c6381f4186f981f0640794f6f217f557d3515a57
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-01-06 14:11:44 +0530

    feat: Added new component

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,9 +1,25 @@
+import Button from "./button";
+
 const App = () => {
     return (
         <div>
-            App
+            <div>
+                <Button></Button>
+            </div>
+            <div>
+                <Button></Button>
+            </div>
+            <div>
+                <Button></Button>
+            </div>
+            <div>
+                <Button></Button>
+            </div>
+            <div>
+                <Button></Button>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default App;
