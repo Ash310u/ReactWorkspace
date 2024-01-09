5f91d107fa0d6e24a240ca641cff0644d2e1134c
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-01-09 12:08:21 +0530

    feat: added some styling

--- comps/src/button.js
+++ comps/src/button.js
@@ -12,7 +12,7 @@ const Button = ({
     rounded
 }) => {
     const classes = twMerge(
-        classnames('px-3 py-1.5 border m-1', {
+        classnames('flex items-center gap-1 px-3 py-1.5 border m-1', {
           'border-blue-500 bg-blue-500 text-white': primary,
           'border-gray-900 bg-gray-900 text-white': secondary,
           'border-green-500 bg-green-500 text-white': success,
