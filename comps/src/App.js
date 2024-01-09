84c544c4c7ce572a7bd26c9250113c84d6829f03
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-01-09 11:11:04 +0530

    feat: Installed Tailwind merge utility library to override the className

--- comps/src/App.js
+++ comps/src/App.js
@@ -4,19 +4,19 @@ const App = () => {
     return (
         <div>
             <div>
-                <Button primary >Click Me!</Button>
+                <Button primary rounded>Click Me!</Button>
             </div>
             <div>
-                <Button secondary>See Deal!</Button>
+                <Button secondary outline >See Deal!</Button>
             </div>
             <div>
-                <Button success>Buy Now!</Button>
+                <Button success rounded outline>Buy Now!</Button>
             </div>
             <div>
                 <Button warning>Hide Ads!</Button>
             </div>
             <div>
-                <Button danger>Feedback!</Button>
+                <Button danger rounded>Feedback!</Button>
             </div>
         </div>
     );
