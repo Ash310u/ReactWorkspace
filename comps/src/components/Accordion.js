aa28740556bf97935d0e962857432bba23cb76c0
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-01-11 00:03:46 +0530

    feat: Added icons

--- comps/src/components/Accordion.js
+++ comps/src/components/Accordion.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { GoChevronDown, GoChevronLeft  } from 'react-icons/go'
 
 const Accordion = ({ items }) => {
     const [expandedIndex, setExpandedIndex] = useState(0)
@@ -7,9 +8,13 @@ const Accordion = ({ items }) => {
     }
     const renderedItems = items.map((item, index) => {
         const isExpanded = index === expandedIndex;
+        const icon = <span>
+            {isExpanded ? <GoChevronDown/> : <GoChevronLeft/> }
+        </span>
+
         return (
             <div key={item.id}>
-                <div onClick={() => handleClick(index)} >{item.label}</div>
+                <div onClick={() => handleClick(index)} >{icon}{item.label}</div>
                 {isExpanded && <div>{item.content}</div>}
             </div>
         )
