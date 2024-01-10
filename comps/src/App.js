4303a8a8fb7fb5efcc583231a975f6b998997e90
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-01-10 14:24:30 +0530

    feat: Added a new component Accordion

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,53 +1,44 @@
-import Button from "./components/Button";
-import { GoFlame, GoDatabase, GoHome, GoCpu, GoCloud } from "react-icons/go";
+import Accordion from "./components/Accordion";
 
 const App = () => {
-    const handleClick = () => {
-            console.log('clicked');
-    }
-    const handleMouseEnter = () => {
-            console.log('Mouse Entered');
-    }
-    const handleMouseLeave = () => {
-            console.log('Mouse Leaved');
-    }
-    const handleMouseOver = () => {
-            console.log('Mouse Overed');
-    }
-    return (
-        <div>
-            <div>
-                <Button primary rounded onClick={handleClick} className='mb-5'>
-                    <GoCloud />
-                    Cloud!
-                </Button>
-            </div>
-            <div>
-                <Button secondary outline onMouseEnter={handleMouseEnter}>
-                    <GoDatabase />
-                    Database!
-                </Button>
-            </div>
-            <div>
-                <Button success rounded outline onMouseLeave={handleMouseLeave}>
-                    <GoHome />
-                    Home!
-                </Button>
-            </div>
-            <div>
-                <Button warning onMouseOver={handleMouseOver}>
-                    <GoCpu />
-                    CPU!
-                </Button>
-            </div>
-            <div>
-                <Button danger rounded>
-                    <GoFlame />
-                    Jwala!
-                </Button>
-            </div>
-        </div>
-    );
+    const items = [
+        {
+            id:1,
+            label: `The Philosopher's Stone (Sorcerer's Stone)`,
+            content:`The revelation of the Philosopher's Stone and its protection by various enchantments. Professor Quirrell's betrayal and Voldemort's return.`
+        },
+        {
+            id:2,
+            label:`The Chamber of Secrets`,
+            content:`The discovery of Tom Riddle's diary and its connection to the Basilisk. Dobby's sacrifice to save Harry.`
+        },
+        {
+            id:3,
+            label:`The Prisoner of Azkaban`,
+            content:`The revelation of Sirius Black's innocence and betrayal by Peter Pettigrew. The time-turner twist, revealing the events that unfolded during the movie.`
+        },
+        {
+            id:4,
+            label:`The Goblet of Fire`,
+            content:`Harry being unexpectedly entered into the Triwizard Tournament. The return of Lord Voldemort to power.`
+        },
+        {
+            id:5,
+            label:`The Order of the Phoenix`,
+            content:`The tragic death of Sirius Black in the Battle of the Department of Mysteries. The prophecy about Harry and Voldemort's connection.`
+        },
+        {
+            id:6,
+            label:`The Half-Blood Prince`,
+            content:`Dumbledore's demise at the hands of Snape. The discovery of Voldemort's Horcruxes.`
+        },
+        {
+            id:7,
+            label:`The Deathly Hallows`,
+            content:`The three Deathly Hallows and their significance. Snape's true allegiance and his love for Lily Potter. The Battle of Hogwarts and the casualties, including Fred Weasley and Remus Lupin.`
+        },
+    ]
+    return <Accordion items={items} />
 };
 
 export default App;
