a3cd65c7c69828054e2ee20ff9b313e9e39bcbae
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-01-11 10:47:50 +0530

    feat: Added a if condition to collapsed the accordion on onClick event

--- comps/src/components/Accordion.js
+++ comps/src/components/Accordion.js
@@ -2,10 +2,16 @@ import { useState } from "react"
 import { GoChevronDown, GoChevronLeft } from 'react-icons/go'
 
 const Accordion = ({ items }) => {
-    const [expandedIndex, setExpandedIndex] = useState(0)
+    const [expandedIndex, setExpandedIndex] = useState(-1)
+    
     const handleClick = (nextIndex) => {
-        setExpandedIndex(nextIndex)
+        if (expandedIndex === nextIndex) {
+            setExpandedIndex(-1)
+        } else {
+            setExpandedIndex(nextIndex)
+        }
     }
+    
     const renderedItems = items.map((item, index) => {
         const isExpanded = index === expandedIndex;
         const icon = <span className="text-2xl">
@@ -14,7 +20,13 @@ const Accordion = ({ items }) => {
 
         return (
             <div key={item.id}>
-                <div className='flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer' onClick={() => handleClick(index)} >{item.label} {icon}</div>
+                <div 
+                    className='flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer' 
+                    onClick={() => handleClick(index)}
+                >
+                    {item.label}
+                    {icon}
+                    </div>
                 {isExpanded && <div className="border-b p-5">{item.content}</div>}
             </div>
         )
