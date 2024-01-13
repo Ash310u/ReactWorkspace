34d3019ac3ca394523a66adee9aef66368259b03
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-01-13 19:26:31 +0530

    refactor: refactor all the code of App.js to DropdownPage.js

---
+++ comps/src/pages/DropdownPage.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import Dropdown from "../components/Dropdown";
+
+const DropdownPage = () => {
+    const [selectedItem, setSelectedItem] = useState(null)
+    const handleSelect = (option) => {
+        setSelectedItem(option)
+    }
+    const options = [
+        { label: 'Red', value: 'red' },
+        { label: 'Blue', value: 'blue' },
+        { label: 'Green', value: 'green' },
+        { label: 'Yellow', value: 'yellow' }
+    ]
+    return (
+        <div className="flex">
+             <Dropdown options={options} onChange={handleSelect} value={selectedItem} />
+             <Dropdown options={options} onChange={handleSelect} value={selectedItem} />
+        </div>
+    )
+};
+
+export default DropdownPage;
