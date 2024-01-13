3a0e19d26805be0bc6db172ceba6d617136091f7
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-01-13 19:27:56 +0530

    refactor:  removed all code from App.js

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,23 +1,5 @@
-import { useState } from "react";
-import Dropdown from "./components/Dropdown";
-
 const App = () => {
-    const [selectedItem, setSelectedItem] = useState(null)
-    const handleSelect = (option) => {
-        setSelectedItem(option)
-    }
-    const options = [
-        { label: 'Red', value: 'red' },
-        { label: 'Blue', value: 'blue' },
-        { label: 'Green', value: 'green' },
-        { label: 'Yellow', value: 'yellow' }
-    ]
-    return (
-        <div className="flex">
-             <Dropdown options={options} onChange={handleSelect} value={selectedItem} />
-             <Dropdown options={options} onChange={handleSelect} value={selectedItem} />
-        </div>
-    )
+    return <div></div>
 };
 
 export default App;
