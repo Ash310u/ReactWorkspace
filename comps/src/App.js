3c14de42e199f061faa73ab2248ec08ccd182af6
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-01-15 03:13:47 +0530

    feat: added a 'Sidebar' comp and some styling

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,16 +1,17 @@
-import Link from './components/Link'
 import Route from './components/Route'
 import DropdownPage from "./pages/DropdownPage";
 import AccordionPage from "./pages/AccordionPage";
+import ButtonPage from "./pages/ButtonPage";
+import Sidebar from './components/Sidebar';
 
 const App = () => {
     return (
         <div>
-            <Link to='/accordion'>Accordion</Link> 
-            <Link to='/dropdown'>Dropdown</Link> 
+            <Sidebar/>
             <div>
                 <Route path='/accordion'><AccordionPage/></Route>
                 <Route path='/dropdown'><DropdownPage/></Route>
+                <Route path='/buttons'><ButtonPage/></Route>
             </div>
         </div>
     )
