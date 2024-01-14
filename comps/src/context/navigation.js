cb94e45b27a8c8833689709e0baa96ee659cdee2
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-01-14 10:07:59 +0530

    feat: Handling user clicking forward and backward when user is navigating between diff addresses that they got to by using that pushstate function. It's not gonna handle the case which user does a hard refresh

--- comps/src/context/navigation.js
+++ comps/src/context/navigation.js
@@ -1,9 +1,24 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
 const NavigationContext = createContext();
 
 const NavigationProvider = ({ children }) => {
+    const [currentPath, setCurrentPath] = useState(window.location.pathname)
+
+    useEffect(() => {
+        const handler = () => {
+            setCurrentPath(window.location.pathname)
+        }
+        // Anytime you add a EventListener to the window object, you always make sure you add up a cleanup function.
+        window.addEventListener('popstate', handler)
+        
+        return () => {
+            window.removeEventListener('popstate', handler)
+        }
+    },[])
+
     return <NavigationContext.Provider value={{}}>
+        {currentPath}
         {children}
     </NavigationContext.Provider>
 }
