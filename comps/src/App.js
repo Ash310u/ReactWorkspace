f26d5ff6b654fff82ea0f4566d83899b28d52d3e
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-01-14 16:55:59 +0530

    feat: Added a 'Link' comp with two props 'to' and 'children' and passed the navigate function from NavigationContext to navigate between diff addresses without any hard refresh

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,5 +1,11 @@
+import Link from './components/Link'
 const App = () => {
-    return <div></div>
+    return (
+        <div>
+            <Link to='/Accordion'>Go to Accordion</Link> 
+            <Link to='/Dropdown'>Go to Dropdown</Link> 
+        </div>
+    )
 };
 
 export default App;
