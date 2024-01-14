cb9aa581eeb05e8bc787c5362a054536da6688a2
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-01-15 02:56:39 +0530

    cleanup: Cleaned up some code

--- comps/src/pages/DropdownPage.js
+++ comps/src/pages/DropdownPage.js
@@ -15,7 +15,6 @@ const DropdownPage = () => {
     return (
         <div className="flex">
              <Dropdown options={options} onChange={handleSelect} value={selectedItem} />
-             <Dropdown options={options} onChange={handleSelect} value={selectedItem} />
         </div>
     )
 };
