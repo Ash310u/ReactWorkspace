f4f93e3e75e55a0fe35a2b58b7f81403f9e56946
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-01-16 11:57:13 +0530

    feat: Added styling and fixed a bug related to scrolling when then modal is open and a cleanup func

--- comps/src/components/Modal.js
+++ comps/src/components/Modal.js
@@ -1,13 +1,28 @@
 import ReactDOM from "react-dom"
 import Button from "./Button"
+import { useEffect } from "react"
 
 const Modal = ({ onClose, children, actionBar }) => {
+    useEffect(() => {
+        // Whenever Modal comp rendered then this class will be appeared" 
+        document.body.classList.add('overflow-hidden');
+        
+        // Cleanup function to remove the class for scrolling after the modal is closed
+        return () => {
+            document.body.classList.remove('overflow-hidden');
+        }
+    },[])
+    
     return ReactDOM.createPortal(
         <div >
             <div onClick={onClose} className="absolute inset-0 bg-gray-300 opacity-80"></div>
             <div className="absolute inset-72 p-10  bg-white">
-                {children}
-                {actionBar}
+                <div className="flex flex-col justify-between">
+                    {children}
+                    <div className="flex justify-end">
+                        {actionBar}
+                    </div>
+                </div>
             </div>
         </div >,
 
