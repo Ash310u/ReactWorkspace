27f1eecfd2f27d1ce896a4f352d6d99ba21a6e9a
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-01-16 16:59:39 +0530

    feat: Added tables to the Table comp with some styling

--- comps/src/pages/TablePage.js
+++ comps/src/pages/TablePage.js
@@ -1,9 +1,16 @@
 import Table from "../components/Table";
 
 const TablePage = () => {
+    const data = [
+        { name: 'Mango', color: 'bg-yellow-400', score: 5 },
+        { name: 'Orange', color: 'bg-orange-400', score: 3 },
+        { name: 'Apple', color: 'bg-red-600', score: 4 },
+        { name: 'Lime', color: 'bg-green-600', score: 2 },
+    ]
+
     return (
         <div>
-            <Table />
+            <Table data={data} />
         </div>
     )
 }
