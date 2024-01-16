2f2cd4a0c2a3c2ec388e8d70de4a6350ae8df480
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-01-16 11:20:13 +0530

    feat: Added a actionBar and a paragraph to the Modal comp

--- comps/src/pages/ModalPage.js
+++ comps/src/pages/ModalPage.js
@@ -4,13 +4,25 @@ import Modal from "../components/Modal";
 
 const ModalPage = () => {
     const [showModal, setShowModal] = useState(false)
+
     const handleClose = () => {
         setShowModal(false)
     }
+
+    const actionBar = <div>
+        <Button onClick={handleClose} primary rounded> I Accept</Button>
+    </div>
+
+    const modal = <Modal onClose={handleClose} actionBar={actionBar}>
+        <p>
+            Here It's a notice from Professor Albus Dumbledore
+        </p>
+    </Modal>
+
     return (
         <div>
             <Button primary rounded onClick={() => setShowModal(true)}>Open Modal</Button>
-            {showModal && <Modal onClose={handleClose}/>}
+            {showModal && modal}
         </div>
     );
 };
