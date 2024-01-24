b2de6946b1af784d14ec2a307f01bcd881434ca2
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-01-25 03:56:50 +0530

    feat: Added states for name and cost in Form comp

--- cart/src/components/Form.js
+++ cart/src/components/Form.js
@@ -1,7 +1,45 @@
+import { useDispatch, useSelector } from "react-redux";
+import { changeCost, changeName } from '../store'
+
 const Form = () => {
+    const dispatch = useDispatch()
+
+    const { name, cost } = useSelector((state) => {
+        return state.form
+    })
+    
+    const handleNameChange = (e) => {
+        dispatch(changeName(e.target.value))
+    }
+    const handleCostChange = (e) => {
+        const cost = parseInt(e.target.value) || 0
+        dispatch(changeCost(cost))
+    }
+
     return (
-        <div>
-            hi from Form
+        <div className="car-form panel">
+            <h4 className="subtitle is-3"> Add Car</h4>
+            <form>
+                <div className="field-group">
+                    <div className="field">
+                        <label className="label">Name</label>
+                        <input
+                            className="input is-expanded"
+                            type="text"
+                            value={name}
+                            onChange={handleNameChange}
+                        />
+                        <input
+                            className="input is-expanded"
+                            type="number"
+                            value={cost === 0 ? '' : cost}
+                            onChange={handleCostChange}
+                        />
+                        <button></button>
+                    </div>
+                </div>
+            </form>
+            <div>{name}, {cost}</div>
         </div>
     )
 }
