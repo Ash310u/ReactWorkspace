ca471fcd10e72c0e6b09e5a0028d73ef9ef32d0e
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-01-25 12:01:29 +0530

    feat: Added 'extraReducers' to check for action on another slice and reset the input value whenever user clicks on add btn

--- cart/src/store/slice/formHorcrux.js
+++ cart/src/store/slice/formHorcrux.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { addData } from './listHorcrux'
 
 const formHorcrux = createSlice({
     name: 'form',
@@ -13,6 +14,12 @@ const formHorcrux = createSlice({
         changeCost(state, action) {
             state.cost = action.payload
         }
+    },
+    extraReducers(builder) {
+    builder.addCase( addData, (state, action) => {
+            state.name = ''
+            state.cost = 0
+        })
     }
 })
 
