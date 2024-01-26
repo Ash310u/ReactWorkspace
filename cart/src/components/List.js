791c70860eee258d984063ef94829e3448cdbb16
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-01-27 00:35:01 +0530

    feat(Bold text when name matches): Whenever the typed name and already saved name matches then the text style is bold

--- cart/src/components/List.js
+++ cart/src/components/List.js
@@ -2,17 +2,25 @@ import { useDispatch, useSelector } from "react-redux";
 import { removeData } from "../store";
 const List = () => {
     const dispatch = useDispatch()
-    const listData = useSelector(({ list: { searchTerm, data } }) => {
-        
-        return data.filter((obj) => 
-            // If the searchTerm is an empty('') string(which actually it is at first), the `includes` function will return `true` for any string in the array since an empty string is present in every string. The `filter` function, in this case, will return a new array containing all elements of the original array.
+    const { filteredData, name } = useSelector(({ form, list: { searchTerm, data } }) => {
+        const filteredData = data.filter((obj) => 
+            // If the searchTerm is an empty('') string(which actually it is at first), the `includes` function will return `true` for any string in the array since an empty string is present in every string.
+            // The `filter` function, in this case, will return a new array containing all elements of the original array.
             obj.name.toLowerCase().includes(searchTerm.toLowerCase())
         )
+        return {
+            filteredData,
+            name: form.name
+        }
     })
 
-    const renderedList = listData.map((obj) => {
+    const renderedList = filteredData.map((obj) => {
+
+        const bold = name && obj.name.toLowerCase().includes(name.toLowerCase())
+        
+
         return (
-            <div key={obj.id} className="panel">
+            <div key={obj.id} className={`panel ${bold && 'bold'}`}>
                 <p>
                     {obj.name} - ${obj.cost}
                 </p>
