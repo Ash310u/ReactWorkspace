47fcb8eff077fd482acd0502be561b151b062cfd
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-01-26 18:00:36 +0530

    feat: Added filter for Searching

--- cart/src/components/List.js
+++ cart/src/components/List.js
@@ -2,8 +2,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { removeData } from "../store";
 const List = () => {
     const dispatch = useDispatch()
-    const listData = useSelector((state) => {
-        return state.list.data
+    const listData = useSelector(({ list: { searchTerm, data } }) => {
+        
+        return data.filter((obj) => 
+            // If the searchTerm is an empty('') string(which actually it is at first), the `includes` function will return `true` for any string in the array since an empty string is present in every string. The `filter` function, in this case, will return a new array containing all elements of the original array.    
+            obj.name.toLowerCase().includes(searchTerm.toLowerCase())
+        )
     })
 
     const renderedList = listData.map((obj) => {
