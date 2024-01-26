c40d1f43636d3276b82a7f8f7120c2b0f8d62e7d
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-01-26 17:07:04 +0530

    feat: Added Search.js comp

--- cart/src/components/Search.js
+++ cart/src/components/Search.js
@@ -1,7 +1,27 @@
+import { useDispatch, useSelector } from "react-redux"
+import { changeSearchTerm } from "../store";
+
 const Search = () => {
+    const dispatch = useDispatch();
+    const searchTerm = useSelector((state) => {
+        return state.list.searchTerm;
+    })
+
+    const handleSearchTermChange = (e) => {
+        dispatch(changeSearchTerm(e.target.value))
+    }
+
     return (
-        <div>
-            hi from Search
+        <div className="list-header">
+            <h3 className="title is-3">Search car</h3>
+            <div className="search field is-horizontal">
+                <label className="label">Search</label>
+                <input 
+                    className="input"
+                    value={searchTerm}
+                    onChange={handleSearchTermChange}
+                />
+            </div>
         </div>
     )
 }
