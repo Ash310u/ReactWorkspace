1ec61dd686ee3f9c39d6f27c89d774a04d19c3a2
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-01-26 23:59:18 +0530

    feat(Added Value comp): Add value component first i using  useSelector to get all the data from my store and set a filter method for counting the searchTerm's price using reduce method

--- cart/src/components/Value.js
+++ cart/src/components/Value.js
@@ -1,7 +1,14 @@
+import { useSelector } from "react-redux"
+
 const Value = () => {
+    const totalCost = useSelector(({ list : { searchTerm , data}}) => {
+        return data.filter((obj) =>
+            obj.name.toLowerCase().includes(searchTerm.toLowerCase())
+        ).reduce((acc, curr) => acc + curr.cost, 0);
+    })
     return (
-        <div>
-            hi from Value
+        <div className="car-value">
+            Total Cost: ${totalCost}
         </div>
     )
 }
