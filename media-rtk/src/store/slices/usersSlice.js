bc3fd8e5864f1e645fc5452c2c56ea3178ef2d10
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-01-28 05:19:24 +0530

    feat(Async Thunk Funcions): use createAsyncThunk with a base of 'users/fetch' create a 'async' function for get the data using 'axios' by sending a '.get' request and set some 'extraReducers' inside 'usersSlice' for responding to 'pending, fulfilled and rejected' actions of the 'Async Thunk function'

--- media-rtk/src/store/slices/usersSlice.js
+++ media-rtk/src/store/slices/usersSlice.js
@@ -1,13 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit"
+import { fetchUsers } from "../thunks/fetchUsers";
 
 const usersSlice = createSlice({
     name: 'users',
-    initialState:{
-        data: [],
+    initialState: {
         isLoading: false,
+        data: [],
         error: null
     },
-    reducers: {}
+    // The reducers property both creates an action creator function and responds to that action in the slice reducer.
+    // The extraReducers allows you to respond to an action in your slice reducer but does not create an action creator function.
+    extraReducers(builder) {
+        builder.addCase(fetchUsers.pending, (state, action) => {
+            state.isLoading = false;
+        });
+        builder.addCase(fetchUsers.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.data= action.payload;
+        });
+        builder.addCase(fetchUsers.rejected, (state, action) => {
+            state.isLoading = false;
+            state.error = action.error;
+        });
+    }
 })
 
 export const usersReducer = usersSlice.reducer;
---
+++ media-rtk/src/store/thunks/fetchUsers.js
@@ -0,0 +1,9 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios'
+
+const fetchUsers = createAsyncThunk('users/fetch', async () => {
+    const response = axios.get('http://127.0.0.1:3005/users');
+    return response.data
+})
+
+export { fetchUsers };
