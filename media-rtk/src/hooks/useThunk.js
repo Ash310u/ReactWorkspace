a0caf858edf183f33d32522695a5c0eadeb41467
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-01-30 04:14:59 +0530

    feat(useThunk custom hook): Create a custom hook 'useThunk' it's handling 'loading' & 'error' using 'unwrap' on 'dispatch' and returns a a 'useCallback'(using 'useCallback' to prevent infinite 're-rendering' of 'useEffect' hook) function 'runThunk', 'isLoading' & 'error' with array destructuring

---
+++ media-rtk/src/hooks/useThunk.js
@@ -0,0 +1,24 @@
+import { useState, useCallback } from 'react'
+import { useDispatch} from "react-redux";
+
+const useThunk = (thunk) => {
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState(null)
+    
+    const dispatch = useDispatch()
+    const runThunk = useCallback(() => {
+        setIsLoading(true)
+        dispatch(thunk())
+            .unwrap() // The "unwrap()" method is used to access the resolved or rejected value of a dispatched asynchronous action ( Basically unwrap() give a brand new promise back )
+            .catch((error) => setError(error))
+            .finally(() => setIsLoading(false));
+    },[dispatch, thunk])
+
+    return [
+        runThunk,
+        isLoading,
+        error
+    ]
+}
+
+export default useThunk;
