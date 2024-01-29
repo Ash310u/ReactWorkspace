cb77e2d9ae2b3fe471352fba3a960fc3861a5ec7
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-01-29 22:40:30 +0530

    feat(UsersList btn setup): Setup the 'Add User' btn inside UsersList & call 'dispatch' with 'addUser' action

--- media-rtk/src/components/UsersList.js
+++ media-rtk/src/components/UsersList.js
@@ -1,7 +1,8 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from "react-redux";
-import { fetchUsers } from '../store';
+import { fetchUsers, addUser } from '../store';
 import Skeleton from './Skeleton';
+import Button from './Button'
 
 const UsersList = () => {
     const dispatch = useDispatch()
@@ -14,19 +15,35 @@ const UsersList = () => {
         dispatch(fetchUsers())
     }, [dispatch])
 
+    const handleUserAdd = ()  => {
+        dispatch(addUser());
+    }
+
     if(isLoading) {
-        return <Skeleton times={10} className="h-10 w-full"/>
+        return <Skeleton times={6} className="h-10 w-full"/>
     }
     if(error) {
         return <div> Error fetching data...</div>;
     }
-    // const usersList = data.map((user) => {
-    //     return <div key={user.id}>{user.name}</div>
-    // })
+    const renderedUsers = data.map((user) => {
+        return (
+            <div key={user.id} className='mb-2 border rounded '>
+                <div className='flex p-2 justify-between items-center cursor-pointer'>
+                    {user.name}
+                </div>
+            </div>
+        )
+    })
 
     return (
         <div>
-            {data.length}
+            <div className='flex flex-row justify-between m-3'>
+                <h1 className='m-2 text-xl'>Users</h1>
+                <Button success rounded onClick={handleUserAdd}>
+                    + Add User
+                </Button>
+            </div>
+            {renderedUsers}
         </div>
     )
 }
