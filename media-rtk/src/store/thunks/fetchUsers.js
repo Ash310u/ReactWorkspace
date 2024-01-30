e7252407b51954049713ae58e122967a9702628f
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-01-31 00:58:50 +0530

    refactor(remove delay): Remove the 10 sec delay

--- media-rtk/src/store/thunks/fetchUsers.js
+++ media-rtk/src/store/thunks/fetchUsers.js
@@ -3,18 +3,8 @@ import axios from 'axios'
 
 const fetchUsers = createAsyncThunk('users/fetch', async () => {
     const response = await axios.get('http://127.0.0.1:3005/users');
-    
-    // for dev purpose
-    await pause(1000)
 
     return response.data
 })
-
-// for dev purpose
-const pause = (ms) => {
-    return new Promise((resolve) => {
-        setTimeout(resolve, ms);
-    })
-}
 
 export { fetchUsers };
