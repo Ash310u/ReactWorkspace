e38fc884e20ff09c94b957ad7d961679d25b997e
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-01-31 02:10:07 +0530

    refactor(useThunk): Add an Argument to the 'runThunk' 'useCallback' func to send some 'argument' inside the 'Thunk' func(And by doing this I'm sending that 'user' object to te thunk func to send 'delete' request to the server)

--- media-rtk/src/hooks/useThunk.js
+++ media-rtk/src/hooks/useThunk.js
@@ -6,9 +6,9 @@ const useThunk = (thunk) => {
     const [error, setError] = useState(null)
     
     const dispatch = useDispatch()
-    const runThunk = useCallback(() => {
+    const runThunk = useCallback((arg) => {
         setIsLoading(true)
-        dispatch(thunk())
+        dispatch(thunk(arg))
             .unwrap() // The "unwrap()" method is used to access the resolved or rejected value of a dispatched asynchronous action ( Basically unwrap() give a brand new promise back )
             .catch((error) => setError(error))
             .finally(() => setIsLoading(false));
