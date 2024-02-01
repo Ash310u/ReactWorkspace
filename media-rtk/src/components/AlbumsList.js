4b8cb6141320a7c7da99a1766f8836dadf3776d1
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-02-01 05:37:33 +0530

    feat(testing useFetchAlbumsQuery): Test 'useFetchAlbumsQuery' inside  AlbumsList comp

--- media-rtk/src/components/AlbumsList.js
+++ media-rtk/src/components/AlbumsList.js
@@ -1,4 +1,8 @@
+import { useFetchAlbumsQuery } from "../store";
+
 const AlbumList = ({ user }) => {
+    const { data, error, isLoading } = useFetchAlbumsQuery(user);
+    console.log(data, error, isLoading)     
     return (
         <div>
             Albums for {user.name}
