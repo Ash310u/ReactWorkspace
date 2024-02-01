8c8a3f42b0d673465f63154024e03d5c4383e48d
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-02-02 01:20:43 +0530

    feat(Setup Skeleton comp & useAddAlbumMutation hook): set 'if-else' for 'Skeleton' comp & Add that 'ExpandablePanel' comp to expand 'Albums' also setup 'useAddAlbumMutation' hook for fetching albums

--- media-rtk/src/components/AlbumsList.js
+++ media-rtk/src/components/AlbumsList.js
@@ -1,11 +1,39 @@
-import { useFetchAlbumsQuery } from "../store";
+import { useAddAlbumMutation, useFetchAlbumsQuery } from "../store";
+import Skeleton from "./Skeleton";
+import ExpandablePanel from "./ExpandablePanel";
+import Button from "./Button";
 
 const AlbumList = ({ user }) => {
     const { data, error, isLoading } = useFetchAlbumsQuery(user);
-    console.log(data, error, isLoading)     
+    const [ addAlbum ] = useAddAlbumMutation();
+
+    const handleAddAlbum = ()  => {
+        addAlbum(user)
+    }
+
+    let content;
+    if (isLoading) {
+        content = <Skeleton times={3} className="h-10 w-full" />
+    } else if (error) {
+        content = <div>Error loading Albums...</div>
+    } else {
+        content = data.map(album => {
+            const header = <div>{album.title}</div>
+            return <ExpandablePanel key={album.id} header={header}>
+                List of Photos in the album
+            </ExpandablePanel>
+        })
+    }
+
     return (
         <div>
-            Albums for {user.name}
+            <div className="flex flex-row justify-between items-center p-2">
+                Albums for {user.name}
+                <Button onClick={handleAddAlbum} secondary rounded>+ Add</Button>
+            </div>
+            <div>
+                {content}
+            </div>
         </div>
     )
 }
