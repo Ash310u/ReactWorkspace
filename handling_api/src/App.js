e1ed7d9765c479954e25efb0c71823aa251377f5
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-03-10 19:42:31 +0530

    feat: update App.js comp

--- handling_api/src/App.js
+++ handling_api/src/App.js
@@ -2,15 +2,17 @@ import ShowUser from "./ShowUser";
 
 const App = () => {
   return (
-    <div className="w-1/2 flex flex-col absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 gap-8">
-      <div className="w-full flex flex-row justify-between items-center">
-        Users
-        <button>Add User</button>
-      </div>
-      <div className="w-full flex flex-col justify-between items-center gap-2">
-        <ShowUser>User1</ShowUser>
-        <ShowUser>User2</ShowUser>
-        <ShowUser>User3</ShowUser>
+    <div className="w-screen h-screen">
+      <div className="w-1/2 h-1/2 flex flex-col absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 gap-8" >
+        <div className="w-full flex flex-row justify-between items-center">
+          Users
+          <button className="bg-gray-400 p-1">Add User</button>
+        </div>
+        <div className="w-full flex flex-col justify-between items-center gap-2 overflow-y-auto">
+          <ShowUser>User1</ShowUser>
+          <ShowUser>User2</ShowUser>
+          <ShowUser>User3</ShowUser>
+        </div>
       </div>
     </div>
   );
