cfad317b20f2d7dc261f832b0e85f81343291562
Ash310u <107404699+Ash310u@users.noreply.github.com>
2024-03-13 04:19:41 +0530

    feat: some small changes

--- handling_api/src/App.js
+++ handling_api/src/App.js
@@ -1,21 +1,14 @@
-import ShowUser from "./ShowUser";
-
 const App = () => {
-  return (
-    <div className="w-screen h-screen">
-      <div className="w-1/2 h-1/2 flex flex-col absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 gap-8" >
-        <div className="w-full flex flex-row justify-between items-center">
-          Users
-          <button className="bg-gray-400 p-1">Add User</button>
+    return (
+        <div className="w-screen h-screen">
+            <div className="w-1/2 h-1/2 flex flex-col absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 gap-8" >
+                <div className="w-full flex flex-row justify-between items-center">
+                    Users
+                    <button className="bg-gray-400 p-1">Add User</button>
+                </div>
+            </div>
         </div>
-        <div className="w-full flex flex-col justify-between items-center gap-2 overflow-y-auto">
-          <ShowUser>User1</ShowUser>
-          <ShowUser>User2</ShowUser>
-          <ShowUser>User3</ShowUser>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
